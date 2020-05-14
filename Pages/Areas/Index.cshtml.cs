f1ae8f51d7f876b67cfa96c1a2ef39181cac063e
Raian Nicacio <raiannicacio@id.uff.br>
2020-05-13 22:39:57 -0300

    Finalização estrutura básica das Áreas

--- Pages/Areas/Index.cshtml.cs
+++ Pages/Areas/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using EmpSys.Data;
+using EmpSys.ScriptsExtras;
 using EmpSys.Models;
 
 namespace EmpSys
@@ -18,12 +18,41 @@ namespace EmpSys
         {
             _context = context;
         }
-
-        public IList<Area> Area { get;set; }
-
-        public async Task OnGetAsync()
+        public string NameSort{get; set;}
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+        public PaginatedList<Area> Areas { get; set; }
+        public async Task OnGetAsync(string sortOrder, string currentFilter,  string searchString, int? pageIndex)
         {
-            Area = await _context.Areas.ToListAsync();
+            CurrentSort = sortOrder;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            CurrentFilter = searchString;
+            IQueryable<Area> areasIQ = from a in _context.Areas select a;
+
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                areasIQ = areasIQ.Where(a => a.name.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    areasIQ = areasIQ.OrderByDescending(a => a.name);
+                    break;
+                default:
+                    areasIQ = areasIQ.OrderBy(a => a.name);
+                    break;
+            }
+            int pageSize = 3;
+            Areas = await PaginatedList<Area>.CreateAsync(areasIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,25 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using EmpSys.Data;
+using EmpSys.Models.DashboardViewsModels;
+using EmpSys.Models;
 
 namespace EmpSys.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly EmpSys.Data.EmpContext _context;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, EmpSys.Data.EmpContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
+        public IList<EmployeeGroup> Employees { get; set; }
+        public async Task OnGetAsync()
         {
-
+            IQueryable<EmployeeGroup> data = from employee in _context.Employees
+                                         group employee by employee.AreaID into areaGroup
+                                         select new EmployeeGroup()
+                                         {
+                                             AreaID = areaGroup.Key,
+                                             EmployeeCount = areaGroup.Count()
+                                         };
+            Employees = await data.AsNoTracking().ToListAsync();
         }
     }
 }
